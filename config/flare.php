799bb36ebb38fc4ecbae2620fc58f2a9f1ed59b4
mxbal <mxbal026@gmail.com>
2022-11-02 14:18:59 +0700

    Update

--- config/flare.php
+++ config/flare.php
@@ -76,6 +76,15 @@ return [
     |
     */
 
+    'reporting' => [
+        'anonymize_ips' => true,
+        'collect_git_information' => false,
+        'report_queries' => true,
+        'maximum_number_of_collected_queries' => 200,
+        'report_query_bindings' => true,
+        'report_view_data' => true,
+    ],
+
     'send_logs_as_events' => true,
-    'collect_git_information' => false,
+
 ];
